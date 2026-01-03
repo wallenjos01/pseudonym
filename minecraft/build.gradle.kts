62f4306d74126c9be5190f22d0955c5da96f559e
wallenjos01 <wallenjos01@gmail.com>
2026-01-03 13:16:22 -0700

    Update to 1.21.11. Add translate, keybind, and some object placeholders

--- minecraft/build.gradle.kts
+++ minecraft/build.gradle.kts
@@ -33,5 +33,5 @@ dependencies {
     }
 
     modImplementation(libs.midnightcfg.minecraft)
-    modImplementation("org.wallentines:databridge:0.9.0")
+    modImplementation("org.wallentines:databridge:0.10.0")
 }
--- minecraft/src/main/java/org/wallentines/pseudonym/mc/impl/ServerPlaceholdersImpl.java
+++ minecraft/src/main/java/org/wallentines/pseudonym/mc/impl/ServerPlaceholdersImpl.java
@@ -1,17 +1,31 @@
 package org.wallentines.pseudonym.mc.impl;
 
+import net.minecraft.core.ClientAsset.ResourceTexture;
+import net.minecraft.data.AtlasIds;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.Style;
+import net.minecraft.network.chat.contents.objects.AtlasSprite;
+import net.minecraft.network.chat.contents.objects.PlayerSprite;
+import net.minecraft.resources.Identifier;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.Util;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.player.PlayerSkin;
+import net.minecraft.world.item.component.ResolvableProfile;
 
+import org.wallentines.mdcfg.ConfigSection;
+import org.wallentines.mdcfg.mc.api.ConfigOps;
 import org.wallentines.pseudonym.*;
 import org.wallentines.pseudonym.lang.LangManager;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.yggdrasil.YggdrasilMinecraftSessionService;
+
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public class ServerPlaceholdersImpl {
 
@@ -63,6 +77,17 @@ public class ServerPlaceholdersImpl {
         GLOBAL_PLACEHOLDERS.register(Placeholder.of("player_name", Component.class,
                 ctx -> ctx.context().getFirst(Player.class).map(Player::getDisplayName)));
 
+        GLOBAL_PLACEHOLDERS.register(Placeholder.of("player_face", Component.class,
+                ctx -> ctx.context().getFirst(Player.class).map(player -> 
+                (Component) Component.object(
+                    new PlayerSprite(ResolvableProfile.createResolved(player.getGameProfile()), true)
+                ))
+                .or(() -> ctx.context().getFirst(UUID.class).map(uuid -> 
+                Component.object(
+                    new PlayerSprite(ResolvableProfile.createUnresolved(uuid), true)
+                )))
+        ));
+
         GLOBAL_PLACEHOLDERS.register(Placeholder.of("entity_uuid", String.class,
                 ctx -> ctx.context().getFirst(Entity.class).map(Entity::getStringUUID)));
 
@@ -70,13 +95,73 @@ public class ServerPlaceholdersImpl {
                 ctx -> ctx.context().getFirst(Entity.class).map(Entity::getDisplayName)));
 
         GLOBAL_PLACEHOLDERS.register(Placeholder.of("entity_dimension", String.class, 
-            ctx -> ctx.context().getFirst(Entity.class).map(ent -> ent.level().dimension().location().toString())));
+            ctx -> ctx.context().getFirst(Entity.class).map(ent -> ent.level().dimension().identifier().toString())));
 
         GLOBAL_PLACEHOLDERS.register(Placeholder.of("server_online_players", String.class,
                 ctx -> ctx.context().getFirst(MinecraftServer.class).map(MinecraftServer::getPlayerCount).map(Object::toString)));
 
         GLOBAL_PLACEHOLDERS.register(Placeholder.of("server_max_players", String.class,
                 ctx -> ctx.context().getFirst(MinecraftServer.class).map(MinecraftServer::getMaxPlayers).map(Object::toString)));
+
+        GLOBAL_PLACEHOLDERS.register(Placeholder.of("translate", Component.class,
+                ctx -> Optional.of(
+                Component.translatable(
+                    MessagePipeline.RESOLVE_STRING.accept(ctx.param(), ctx.context())
+                )), ParameterTransformer.IDENTITY));
+
+        GLOBAL_PLACEHOLDERS.register(Placeholder.of("keybind", Component.class,
+                ctx -> Optional.of(
+                Component.keybind(
+                    MessagePipeline.RESOLVE_STRING.accept(ctx.param(), ctx.context())
+                )), ParameterTransformer.IDENTITY));
+
+        GLOBAL_PLACEHOLDERS.register(Placeholder.of("item", Component.class,
+                ctx -> {
+                    try {
+                        return Optional.of(
+                            Component.object(
+                                new AtlasSprite(
+                                    AtlasIds.ITEMS,
+                                    Identifier.tryParse(MessagePipeline.RESOLVE_STRING.accept(ctx.param(), ctx.context()))
+                                )
+                            )
+                        );
+                    } catch(IllegalArgumentException ex) {
+                        return Optional.empty();
+                    }
+                }, ParameterTransformer.IDENTITY));
+        
+        GLOBAL_PLACEHOLDERS.register(Placeholder.of("block", Component.class,
+                ctx -> {
+                    try {
+                        return Optional.of(
+                            Component.object(
+                                new AtlasSprite(
+                                    AtlasIds.BLOCKS,
+                                    Identifier.tryParse(MessagePipeline.RESOLVE_STRING.accept(ctx.param(), ctx.context()))
+                                )
+                            )
+                        );
+                    } catch(IllegalArgumentException ex) {
+                        return Optional.empty();
+                    }
+                }, ParameterTransformer.IDENTITY));
+
+        GLOBAL_PLACEHOLDERS.register(Placeholder.of("face_texture", Component.class,
+                ctx -> { 
+                try {
+                    ConfigSection encoded = new ConfigSection()
+                        .with("texture", MessagePipeline.RESOLVE_STRING.accept(ctx.param(), ctx.context()));
+                    ResolvableProfile profile = ResolvableProfile.CODEC.decode(ConfigOps.INSTANCE, encoded).getOrThrow().getFirst();
+                    return Optional.of(
+                        Component.object(
+                            new PlayerSprite(profile, false)
+                        )
+                    );
+                } catch(IllegalArgumentException ex) {
+                    return Optional.empty();
+                }
+            }, ParameterTransformer.IDENTITY));
 
         LangManager.registerPlaceholders(GLOBAL_PLACEHOLDERS);
     }
